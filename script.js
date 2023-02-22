7baae2803b0ffbff7b34ec3a5d7c656455ff0a93
Elior Yifrach <yifrach1992@gmail.com>
2023-02-23 01:24:46 +0200

    change color and header according to result

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ function setItemData(itemJSON) {
     document.getElementById("itemName").innerText = itemJSON.hebrewName;
     document.getElementById("itemBarcode").innerText = itemJSON.barcode;
     document.getElementById("itemInfo").innerText = itemJSON.info;
+
+    resultHeaderClasses = "w3-panel ";
+    resultCardClasses = "w3-card-2 w3-margin ";
+
+    var isKosher = false;
+
+    itemJSON.info.forEach(addInfo => {
+        if (addInfo === "כשר לפסח") {
+
+            isKosher = true;
+            resultHeaderClasses += "w3-green"
+            resultCardClasses += "w3-light-green"
+            document.getElementById("resultHeader").innerText = "כשר לפסח";
+        }
+    });
+
+    if ( isKosher === false ) {
+        resultHeaderClasses += "w3-red";
+        resultCardClasses += "w3-pale-red";
+        document.getElementById("resultHeader").innerText = "אינו כשר לפסח";
+    }
+
+    document.getElementById("resultCard").className = resultCardClasses;
+    document.getElementById("resultHeader").className = resultHeaderClasses;
 }
 
 function checkItem() {
