c59d835001b0dd05296ee202c64010798624e60e
Elior Yifrach <yifrach1992@gmail.com>
2023-02-23 01:04:22 +0200

    more fixes

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const db = "https://mc5qu4r3.github.io/passover/db/";
+const db = "https://mc5qu4r3.github.io/passover/db";
+
+function setItemData(itemJSON) {
+
+    document.getElementById("itemName").innerText = itemJSON.hebrewName;
+    document.getElementById("itemBarcode").innerText = itemJSON.barcode;
+    document.getElementById("itemInfo").innerText = itemJSON.info;
+}
 
 function checkItem() {
 
     var barcodeValue = document.getElementById("barcodeValue").value;
 
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            document.getElementById("itemData").innerText = xmlHttp.responseText;
-    }
-    xmlHttp.open("GET", `${db}/${barcodeValue}`, true); // true for asynchronous 
-    xmlHttp.send(null);   
+    fetch(`${db}/${barcodeValue}.json`, {
+        method: 'GET',
+        headers: {'Accept': 'application/json',},
+    })
+    .then(response => response.json())
+    .then(jsonData => setItemData(jsonData));
 }
