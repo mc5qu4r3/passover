ead2b457ddb722adf3b8d96aef347f9101b947aa
Elior Yifrach <yifrach1992@gmail.com>
2023-02-22 23:07:24 +0200

    fix query

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const db = "https://mc5qu4r3.github.io/passover/db/";
 
-function checkItem(barcode) {
+function checkItem() {
+
+    var barcodeValue = document.getElementById("barcodeValue").innerText;
 
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             document.getElementById("itemData").innerText = xmlHttp.responseText;
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.open("GET", `${db}/${barcodeValue}`, true); // true for asynchronous 
     xmlHttp.send(null);   
 }
